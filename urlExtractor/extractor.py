34245283ed1a1ea7762b4ac7dc2580f5f5845da2
John <john0payment@gmail.com>
2016-07-24 13:44:36 -0700

    Added urlExtractor

---
+++ urlExtractor/extractor.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+if len(sys.argv) > 1:
+	url = sys.argv[1]
+	r = requests.get(url)
+	data = r.text
+
+	soup = BeautifulSoup(data)
+
+	for link in soup.find_all('a'):
+		print(link.get('href'))
+
